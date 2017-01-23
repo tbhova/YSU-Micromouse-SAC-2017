e18b994e9bc4269b7f8e4e90a074d90fa7894a73
MrFobwatch <kcy0511@gmail.com>
2017-01-23 14:40:50 -0500

    Navigator Outline
    Updated the Navigator Class with descriptions of intended functionality.

--- src/src/navigator.h
+++ src/src/navigator.h
@@ -11,18 +11,34 @@
  */
 
 class Navigator {
-public:
-    Navigator();
-    void run();
-
-protected:
-    void map();
-    void optimalRoute();
-
 private:
+    //Memeber Data
     Driver driver;
     GPS directions;
     Maze maze;
+
+    void updateMaze(); //Get wall data from Driver and adjust maze
+
+    Coordinate findUnvisitedCell();
+        //Search maze for a cell.visited = false
+        //determine coordinates of cell
+
+public:
+    Navigator();
+
+    //this will be called by main - findUnvisitedCell with maze,
+    //use GPS to find direction to that cell, inform driver of direction
+    //Loop this until all cells are visited
+
+    void run();
+
+
+protected:
+
+    void map();
+    void optimalRoute();
+
+
 };
 
 #endif // NAVIGATOR_H
