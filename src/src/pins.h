55afb5bea50d51ec3c5ef13992c64e1548cfc356
Tyler Hovanec <tbhovanec@student.ysu.edu>
2017-01-22 20:11:17 -0500

    Updated pins to be correct

--- src/src/pins.h
+++ src/src/pins.h
@@ -2,21 +2,22 @@
 #define PINS_H
 
 //Digital Pins
-const int   LEFT_MOTOR_FORWARD      = 5,
-            LEFT_MOTOR_REVERSE      = 6,
-            RIGHT_MOTOR_FORWARD     = 7,
-            RIGHT_MOTOR_REVERSE     = 8,
-            LEFT_MOTOR_SPEED        = 9,
+const int   LEFT_MOTOR_SPEED        = 7,
+            LEFT_MOTOR_FORWARD      = 8,
+            LEFT_MOTOR_REVERSE      = 9,
             RIGHT_MOTOR_SPEED       = 10,
-            LEFT_MOTOR_ENCODER_A    = 37,
-            LEFT_MOTOR_ENCODER_B    = 38,
+            RIGHT_MOTOR_FORWARD     = 11,
+            RIGHT_MOTOR_REVERSE     = 12,
+            MOTOR_STANDBY           = 13,
+            LEFT_MOTOR_ENCODER_A    = 38,
+            LEFT_MOTOR_ENCODER_B    = 37,
             RIGHT_MOTOR_ENCODER_A   = 27,
             RIGHT_MOTOR_ENCODER_B   = 28;
 
 //Analog Input Pins
-#define IR_SENSOR_LEFT      A13 //pin 32
+#define IR_SENSOR_RIGHT     A13 //pin 32
 #define IR_SENSOR_CENTER    A14 //pin 33
-#define IR_SENSOR_RIGHT     A15 //pin 34
+#define IR_SENSOR_LEFTT     A15 //pin 34
 
 
 #endif // PINS_H
