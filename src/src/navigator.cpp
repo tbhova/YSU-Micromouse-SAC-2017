f722763f75101606ca67d7d42ad1004ffcacaeff
PhillMyCup <phillguidos@gmail.com>
2017-02-08 15:10:16 -0500

    Changes to gps and navigator
    GPS - Added askForDirections
    Navigator - updated find unvisited cell

--- src/src/navigator.cpp
+++ src/src/navigator.cpp
@@ -3,9 +3,9 @@
 Navigator::map() {
         for (int x = 0; x < sizeX; x++) { //This line should be fixed
             for (int y = 0; y < sizeY; y++) //As is this line
-                if (!maze.isVisited(x,y) && notDestination (7,7 7,8 8,7 8,8)){
+                if (!maze.isVisited(x,y) && notDestination (7,7 7,8 8,7 8,8)){ //What is notDestination
                     //I am unsure of what needs to be done to this statement
-                    find box(x,y)
+                    findbox(x,y)
                     break;
                 }
         }
