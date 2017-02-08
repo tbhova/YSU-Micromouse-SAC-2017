29821c552f13c47a9aeba017b602e09fc15dddc4
Unknown <kcy0511@gmail.com>
2017-02-08 15:16:38 -0500

    Updated Navigator Classes to new code
    implemented changes from Maze class

--- src/src/navigator.cpp
+++ src/src/navigator.cpp
@@ -1,18 +1,18 @@
 #include "navigator.h"
 
-Navigator::map() {
+Coordinate Navigator::findUnvisitedCell() {
         for (int x = 0; x < sizeX; x++) { //This line should be fixed
             for (int y = 0; y < sizeY; y++) //As is this line
-                if (!maze.isVisited(x,y) && notDestination (7,7 7,8 8,7 8,8)){ //What is notDestination
-                    //I am unsure of what needs to be done to this statement
-                    findbox(x,y)
+                if (maze.hasMouseVisited((x,y) && notDestination (7,7 7,8 8,7 8,8)){
+                    //I am unsure of what else needs to be done to this statement
+                    find box(x,y)
                     break;
                 }
         }
     }
-
+}
     findBox(x, y) { //Does this go here?
         updateMaze();
-        Cardinal8 direction = GPS.askForDirectionToXY(curX, CurY, x, y);
+        Cardinal8 direction = GPS.askForDirectionToXY(CurX, CurY, x, y);
     }
 }
