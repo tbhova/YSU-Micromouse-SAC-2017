57e0774b22ab48a4fb3a16213a3a6030c638d2ab
Tyler Hovanec <tbhovanec@student.ysu.edu>
2017-02-07 21:56:08 -0500

    Added maze documentation

--- src/src/maze.h
+++ src/src/maze.h
@@ -5,39 +5,116 @@
 #include <vector>
 #include "coordinate.h"
 
+/**
+ * @brief The Maze class
+ * Keeps track of walls and which cells the mouse (and traversal algorithm) has visited
+ */
 class Maze {
 
 private:
+    /**
+     * @brief The Cell class
+     * 1 cell in the maze
+     * A cell has 4 walls (North, South, East, West)
+     * Keeps tracks of whether the maze or solver has visited the cell
+     */
 	class Cell {
-
 	private:
+        // Order of bits from least to most significant is: North, West, South, East
 		char walls : 4;
+
+        // true is mouse has been to this cell
         bool visited : 1;
+        // true if current graph traversal has seen this cell
         bool traversalVisited : 1;
 
 	public:
+        /**
+         * @brief Cell
+         * Constructor - set up initial state of no walls and not visited
+         */
 		Cell();
+
+        /**
+         * @brief placeWall - adds a wall in the direction given
+         * @param dir - Selects which wall to place - North, South, West, East
+         */
         void placeWall(const Cardinal8 dir);
+
+        /**
+         * @brief removeWall - removes a wall from the cell in the given direction
+         * @param dir - Which wall (NSWE) to remove
+         */
         void removeWall(const Cardinal8 dir);
+
+        /**
+         * @brief isWall - checks whether there is a wall in "dir" direction
+         * @param dir - Wall to check for (NSWE)
+         * @return true if there is a wall, false otherwise
+         */
         bool isWall(const Cardinal8 dir) const;
 
+        /**
+         * @brief isVisited - checks if the mouse has been to this cell
+         * @return true if the mouse has visited this cell
+         */
         bool isVisited() const { return visited; }
+        /**
+         * @brief setVisited - Mark this cell as visited by the mouse
+         */
         void setVisited() { visited = true; }
 
+        /**
+         * @brief isTraversalVisited - checks whether the maze solver has seen this cell
+         * @return true if the maze solver has seen this cell
+         */
         bool isTraversalVisited() const { return traversalVisited; }
+        /**
+         * @brief setTraversalVisited - sets maze solver visited property of the cell
+         * @param val - True -> has visited this cell, False -> has not visited
+         */
         void setTraversalVisited(const bool val) { traversalVisited = val; }
 	};
 
 	int sizeX, sizeY;
 	std::vector<std::vector<Cell>> mazeCells = std::vector<std::vector<Cell>>();
 
+    /**
+     * @brief cardinalToBit converts a primary cardinal direction to an 8 bit number
+     * @param dir - primary cardinal direction NSWE
+     * @return int with 1 of the first 4 bits set
+     */
     static int cardinalToBit(const Cardinal8 dir);
+
+    /**
+     * @brief adjacentCell - get the coordinates of the cell to the NSWE of given cell
+     * @param x - given cell's x coordinate
+     * @param y - given cell's y coordinate
+     * @param dir - primary cardinal NSWE
+     * @return int with 1 of lower 4 bits set
+     *
+     * North -> least significant bit (LSB)
+     * West -> 2nd LSB
+     * South -> 3rd LSB
+     * East -> 4th LSB
+     */
     static Coordinate adjacentCell(const int x, const int y, const Cardinal8 dir);
 
+    /**
+     * @brief isValidCell - checks if coordinates are in the maze
+     * @param cell - coordinates of cell
+     * @return true if the coordinates are in the maze
+     */
     bool isValidCell(const Coordinate cell) const;
     bool isValidCell(const int x, const int y) const;
 
     //We need 2 copies of these so that we may call the method both from const and non const methods
+    /**
+     * @brief getCell return the cell at x, y
+     * @param x - x coordinate
+     * @param y - y coordinate
+     * @return Reference to the cell at x,y
+     */
     const Cell& getCell(const int x, const int y) const;
     const Cell& getCell(const Coordinate cell) const;
     Cell& getCell(const int x, const int y);
@@ -45,27 +122,89 @@ private:
 
 
 public:
+    /**
+     * @brief Maze - Constructor, adds outer perimeter walls
+     * @param sizeX - x dimension of the maze
+     * @param sizeY - y dimension of the maze
+     */
 	Maze(const int sizeX, const int sizeY);
 
 	int getSizeX() const { return sizeX; }
 	int getSizeY() const { return sizeY; }
 
+    /**
+     * @brief placeWall - puts the "dir" wall at location x, y
+     * Does nothing if there is already a wall there
+     * @param x - x coordinate of cell to place a wall at
+     * @param y - y coordinate of cell to place a wall at
+     * @param dir - Wall to place (NSWE)
+     */
     void placeWall(const int x, const int y, const Cardinal8 dir);
+
+    /**
+     * @brief removeWall - removes the "dir" wall from location x, y
+     * Does nothing if there is no wall
+     * @param x - x coordinate of cell to remove wall
+     * @param y - y coordinate of cell to remove wall
+     * @param dir - which wall to remove (NSWE)
+     */
     void removeWall(const int x, const int y, const Cardinal8 dir);
+
+    /**
+     * @brief isWall - checks whether there is a "dir" wall at location x, y
+     * @param x - x coordinate of cell to check
+     * @param y - y coordinate of cell to check
+     * @param dir - wall to check for (NSWE)
+     * @return true if there is a wall
+     */
     bool isWall(const int x, const int y, const Cardinal8 dir) const;
     bool isWall(const int x1, const int y1, const int x2, const int y2) const;
 
+    /**
+     * @brief getNeighboringCells - get a list of reachable (no wall) adjacent cells
+     * @param x - x coordinate of cell to check
+     * @param y - y coordinate of cell to check
+     * @return List of all cells that are reachable from x, y
+     */
     std::vector<Coordinate> getNeighboringCells(const int x, const int y) const;
 
+    /**
+     * @brief hasMouseVisited - checks if the mouse has visited x, y
+     * @param x - x coordinate
+     * @param y - y coordinate
+     * @return true if the mouse has visited x, y
+     */
     bool hasMouseVisited(const int x, const int y) const;
     bool hasMouseVisited(const Coordinate cell) const;
+
+    /**
+     * @brief setMouseVisited - mark cell x, y as visited by the mouse
+     * @param x - x coordinate
+     * @param y - y coordinate
+     */
     void setMouseVisited(const int x, const int y);
     void setMouseVisited(const Coordinate cell);
 
+    /**
+     * @brief hasTraversalVisited - checks if the maze solve has seen cell x, y
+     * @param x - x coordinate
+     * @param y - y coordinate
+     * @return true if the maze solver has seen this cell
+     */
     bool hasTraversalVisited(const int x, const int y) const;
     bool hasTraversalVisited(const Coordinate cell) const;
+
+    /**
+     * @brief setTraversalVisited - mark cell x,y as seen by the maze solver
+     * @param x - x coordinate
+     * @param y - y coordinate
+     */
     void setTraversalVisited(const int x, const int y);
     void setTraversalVisited(const Coordinate cell);
+
+    /**
+     * @brief resetTraversalVisited - mark all cells as not visited by maze solver
+     */
     void resetTraversalVisited();
 };
 
