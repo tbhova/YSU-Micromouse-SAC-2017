7f1a6eace32fedf342b0f05d805c6399fdea2f0d
Unknown <kcy0511@gmail.com>
2017-02-22 15:50:58 -0500

    Create Intial Navigator Test

--- tests/auto/micromouse/navigator_test.h
+++ tests/auto/micromouse/navigator_test.h
@@ -1,4 +1,22 @@
 #ifndef NAVIGATOR_TEST_H
 #define NAVIGATOR_TEST_H
 
+#include <gtest/gtest.h>
+#include <gmock/gmock-matchers.h>
+
+#include "../../src/src/maze.h"
+#include "../../src/src/maze.cpp"
+#include "../../src/src/coordinate.h"
+#include "../../src/src/navigator.h"
+#include "../../src/src/navigator.cpp"
+
+#include <vector>
+
+using namespace testing;
+using namespace std;
+
+class NavigatorTest : public ::testing::Test {
+    
+};
+
 #endif // NAVIGATOR_TEST_H
