53dbd885509e0dc9d57d19c9702d17615f69b7bb
MrFobwatch <kcy0511@gmail.com>
2017-03-13 20:56:11 -0400

    Fixed Issue with wallData

--- src/src/hardwaremanager.cpp
+++ src/src/hardwaremanager.cpp
@@ -5,6 +5,6 @@ bool* HardwareManager::areWalls() {
     bool leftWall = irArray.getLeftIR()>8000;
     bool centerWall = irArray.getCenterIR()>8000;
     bool rightWall = irArray.getRightIR()>8000;
-    bool wallData[3] = {leftWall, centerWall, rightWall};
+    static bool wallData[3] = {leftWall, centerWall, rightWall};
     return wallData;
 }
