037fc19892db1989b88104856c1d6bf50f5b95e7
Tyler Hovanec <tbhovanec@gmail.com>
2017-03-22 15:27:43 -0400

    Updated source references

--- tests/auto/micromouse/driver_test.h
+++ tests/auto/micromouse/driver_test.h
@@ -5,20 +5,25 @@
 #include <gmock/gmock-matchers.h>
 #include <gmock/gmock.h>
 
-#include "../../src/src/coordinate.h"
-#include "../../src/src/cardinal8.h"
-#include "../../src/src/abstractdriver.h"
-#include "../../src/src/driver.h"
-#include "../../src/src/driver.cpp"
-#include "../../src/src/stopgodriver.h"
-#include "../../src/src/stopgodriver.cpp"
-#include "../../src/src/hardwaremanager.cpp"
-#include "../../src/src/infraredsensorarray.cpp"
-#include "../../src/src/infraredsensor.cpp"
+#include "coordinate.h"
+#include "cardinal8.h"
+#include "abstractdriver.h"
+#include "driver.h"
+#include "stopgodriver.h"
 
 
 using namespace testing;
 using namespace std;
+
+class MockHwManager: public HardwareManager {
+public:
+    MockDriver() {}
+    MOCK_METHOD1(drive, void(std::stack<Cardinal8> &directions));
+    MOCK_METHOD2(drive, void(const Cardinal8 dir, const int cells));
+    MOCK_METHOD1(drive, void(const Cardinal8 dir));
+    MOCK_METHOD0(getCurrentLocation, Coordinate());
+    MOCK_METHOD0(getWalls, std::vector<Cardinal8>());
+};
 
 class DriverTest : public ::testing::Test {
 protected:
@@ -31,9 +36,11 @@ TEST_F(DriverTest, locationTest) {
 }
 TEST_F(DriverTest, getVarTest) {
     driver.drive(North);
-    EXPECT_EQ(driver.getX(), 0);
+    driver.drive(East);
+    driver.drive(East);
+    EXPECT_EQ(driver.getX(), 2);
     EXPECT_EQ(driver.getY(), 1);
-
+    EXPECT_EQ(driver.getDirection(), East);
 }
 
 
