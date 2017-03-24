d92cc5dc877dcc0eea12547cd2083e9055478b70
PhillMyCup <phillguidos@gmail.com>
2017-03-24 16:42:26 -0400

    Update Main

--- src/src/main.cpp
+++ src/src/main.cpp
@@ -2,9 +2,14 @@
 #include "mk20dx128.h"
 #include "core_pins.h"
 #include "pins.h"
+#include "infraredsensorarray.h"
 
 //#include "motors.h"
-
+extern "C"{
+   int _getpid(){ return -1;}
+   int _kill(int pid, int sig){ return -1; }
+   int _write(){return -1;}
+}
 void setup() {
         pinMode(LED_BUILTIN, OUTPUT);
         pinMode(LEFT_MOTOR_FORWARD, OUTPUT);
@@ -17,9 +22,12 @@ void setup() {
         pinMode(IR_SENSOR_LEFT, INPUT);
         pinMode(IR_SENSOR_CENTER, INPUT);
         pinMode(IR_SENSOR_RIGHT, INPUT);
+        Serial.begin(9600);
 }
 
 void loop() {
+
+        InfraredSensorArray irSensor;
         digitalWriteFast(LED_BUILTIN, HIGH);
         delay(3000);
         digitalWriteFast(LED_BUILTIN, LOW);
@@ -28,5 +36,7 @@ void loop() {
         //motors.setSpeed(128,-128);
         //delay(600);
         //motors.setSpeed(-128,128);
-
+        Serial.print(irSensor.getLeftDistance());
+        Serial.print(irSensor.getCenterDistance());
+        Serial.print(irSensor.getRightDistance());
 }
