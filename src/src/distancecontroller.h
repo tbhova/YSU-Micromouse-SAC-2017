da3b5acb39f550d148462fa1f814322d5fee5afb
Tyler Hovanec <tbhovanec@gmail.com>
2017-03-31 15:51:41 -0400

    Started distance controller

--- src/src/distancecontroller.h
+++ src/src/distancecontroller.h
@@ -6,7 +6,15 @@
 
 class DistanceController {
 public:
-    DistanceController();
+    DistanceController(const int maxVelocity);
+    void updateSetPoint(const int distanceSetPoint);
+    int getNewVelocity(const int distanceTraveled);
+private:
+    const int targetStraightSpeed;
+
+    double distanceInput, distanceSetPoint, velocityOutput;
+
+    PID distancePID = PID(&distanceInput, &velocityOutput, &distanceSetPoint, 0.5, 1.0, 1.25, DIRECT);
 };
 
 #endif // DISTANCECONTROLLER_H
