2d86fea03916feace15345219cbc3b914b4f377b
PhillMyCup <phillguidos@gmail.com>
2017-04-01 18:13:43 -0400

    Add encoder testing
    change variables in the initialization

--- src/src/main.cpp
+++ src/src/main.cpp
@@ -2,12 +2,21 @@
 #include "mk20dx128.h"
 #include "core_pins.h"
 #include "pins.h"
+#include "encoders.h"
+#include "motors.h"
 
 extern "C"{
    int _getpid(){ return -1;}
    int _kill(int pid, int sig){ return -1; }
    int _write(){return -1;}
 }
+int distance=8000;
+int motorSpeed=0;
+int state=1;
+Motors motor;
+Encoders encoder;
+const double wheelBase=95;
+double circles=.25;
 
 void setup() {
     pinMode(LED_BUILTIN, OUTPUT);
@@ -24,10 +33,40 @@ void setup() {
     Serial.begin(9600);
     //while(!Serial);
 }
-
+double radTurn=25*wheelBase*(2*circles*3.4159265359);
 void loop() {
     digitalWriteFast(LED_BUILTIN, HIGH);
-    delay(300);
+    delay(30);
     digitalWriteFast(LED_BUILTIN, LOW);
-    delay(300);
+    delay(30);
+    Serial.println(encoder.getRightTicks());
+    motor.setSpeed(-170, 170);
+    if(encoder.getRightTicks()>=radTurn) {
+        motor.setSpeed(0,0);
+    }
+
+
+//    if(state==1) {
+//        motorSpeed++;
+//        motor.setSpeed(-motorSpeed, motorSpeed);
+//        if(motorSpeed == 130) {
+//            state++;
+//        }
+//    }
+//    if(state==2) {
+//        if(distance-encoder.getLeftTicks() < 3000) {
+//            state++;
+//        }
+//    }
+//    if(state==3) {
+//        motorSpeed--;
+//        motor.setSpeed(-motorSpeed, motorSpeed);
+//        if (motorSpeed == 0) state++;
+//    }
+//    if(state==4) {
+//        Serial.begin(9600);
+//        delay(500);
+//        Serial.println(encoder.getLeftTicks());
+//    }
+
 }
