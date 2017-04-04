3f83a387978c5c10224884c82e424ead892f086a
aphifer <aphifer@student.ysu.edu>
2017-04-04 17:32:32 -0400

    Tuned wall controller

--- src/src/wallcontroller.h
+++ src/src/wallcontroller.h
@@ -21,7 +21,7 @@ private:
     // variable outputs
     double wallOmega = 0;
 
-    PID wallPID = PID(&wallInput, &wallOmega, &wallSetPoint, .01, 0.0, 0.0, DIRECT);
+    PID wallPID = PID(&wallInput, &wallOmega, &wallSetPoint, 0.0049, 0.0001, 0.0/*.01, 0.0, 0.0001*/, DIRECT);
 };
 
 #endif // WALLCONTROLLER_H
